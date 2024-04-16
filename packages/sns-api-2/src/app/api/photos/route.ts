428d4c24494c5c5568c6c28d864796f4d5ebdab5
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-04-16 09:17:31 +0900

    🎉 biome導入

--- packages/sns-api-2/src/app/api/photos/route.ts
+++ packages/sns-api-2/src/app/api/photos/route.ts
@@ -1,16 +1,16 @@
-import { prisma } from "@/lib/prisma";
-import { getPagination, getPaginationSrc } from "@/lib/util/pagination";
-import type { NextRequest } from "next/server";
+import { prisma } from '@/lib/prisma'
+import { getPagination, getPaginationSrc } from '@/lib/util/pagination'
+import type { NextRequest } from 'next/server'
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const page = Number(searchParams.get("page") || "1");
-  const take = Number(searchParams.get("take") || "10");
-  const authorId = searchParams.get("authorId");
-  if (isNaN(page) || isNaN(take)) {
-    return Response.json({ message: "Invalid Params" }, { status: 400 });
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = new URL(request.url)
+  const page = Number(searchParams.get('page') || '1')
+  const take = Number(searchParams.get('take') || '10')
+  const authorId = searchParams.get('authorId')
+  if (Number.isNaN(page) || Number.isNaN(take)) {
+    return Response.json({ message: 'Invalid Params' }, { status: 4000 })
   }
-  const { skip, currentPage } = getPaginationSrc({ page, take });
+  const { skip, currentPage } = getPaginationSrc({ page, take })
   // FYI: https://github.com/prisma/prisma/discussions/3087#discussioncomment-39983
   const [hitCount, photos] = await Promise.all([
     prisma.photo.count({ ...(authorId && { where: { authorId } }) }),
@@ -18,13 +18,13 @@ export async function GET(request: NextRequest) {
       ...(authorId && { where: { authorId } }),
       take,
       skip,
-      orderBy: { createdAt: "desc" },
+      orderBy: { createdAt: 'desc' },
       include: { _count: { select: { likes: true } } },
     }),
-  ]);
+  ])
   console.log(
     `GET: /api/photos?${searchParams.toString()} ${new Date().toISOString()}`,
-  );
+  )
   return Response.json({
     photos: photos.map(({ _count, ...photo }) => ({
       ...photo,
@@ -36,12 +36,12 @@ export async function GET(request: NextRequest) {
       currentPage,
       hitCount,
     }),
-  });
+  })
 }
 
-export async function POST(req: NextRequest) {
+export const POST = async (req: NextRequest) => {
   try {
-    const body = await req.json();
+    const body = await req.json()
     const photo = await prisma.photo.create({
       data: {
         title: body.title,
@@ -50,9 +50,9 @@ export async function POST(req: NextRequest) {
         authorId: body.userId,
         categoryId: body.categoryId,
       },
-    });
-    return Response.json({ photo }, { status: 201 });
+    })
+    return Response.json({ photo }, { status: 201 })
   } catch (err) {
-    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+    return Response.json({ message: 'Internal Server Error' }, { status: 500 })
   }
 }
