b0388b07a7c467a082b675bc02c5b60398dc49dd
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-04-16 09:50:09 +0900

    🎉biome導入

--- packages/sns-web-2/next.config.mjs
+++ packages/sns-web-2/next.config.mjs
@@ -3,16 +3,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
+        protocol: 'http',
+        hostname: '127.0.0.1',
       },
     ],
   },
-  transpilePackages: ["sns-shared-ui"],
-};
+  transpilePackages: ['sns-shared-ui'],
+}
 
-export default nextConfig;
+export default nextConfig
